43c336dab19b3c321a5963413f2c28732505e6bf
Christian Carnalla <ccarnalla4514@stu.d214.org>
2021-01-25 14:44:19 -0600

    Initial Commit

---
+++ SeguesLecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeguesLecture
+//
+//  Created by Christian Carnalla on 1/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
